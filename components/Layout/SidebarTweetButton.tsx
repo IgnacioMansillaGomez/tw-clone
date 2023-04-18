6588d6fb9c8c5e0ae253288697a531676a0041a2
IgnacioMansillaGomez <ignaciomansilladeveloper@gmail.com>
2023-04-18 03:20:09 -0300

    added sidebar logout button

---
+++ components/Layout/SidebarTweetButton.tsx
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+import React from "react";
+import { FaFeather } from "react-icons/fa";
+
+export default function SidebarTweetButton() {
+  const router = useRouter();
+  return (
+    <div onClick={() => router.push("/")}>
+      <div
+        className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex 
+      items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer"
+      >
+        <FaFeather size={24} color="white" />
+      </div>
+    </div>
+  );
+}
