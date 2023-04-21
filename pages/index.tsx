7c4927f0768f78b17125a14660f678cde49c4235
IgnacioMansillaGomez <ignaciomansilladeveloper@gmail.com>
2023-04-20 21:21:38 -0300

    styling sidebarTweetButton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-3xl text-sky-500">
-      <h1>Hello</h1>
-    </div>
-  )
+    <>
+      <Header label="Home" />
+    </>
+  );
 }
