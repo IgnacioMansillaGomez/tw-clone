990d1e86e77eef1be61a1f6b501e61a1b53cfca9
IgnacioMansillaGomez <ignaciomansilladeveloper@gmail.com>
2023-04-21 00:06:50 -0300

    create model as wrapper

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import React, { useCallback } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
 interface HeaderProps {
   label?: string;
@@ -15,7 +16,17 @@ const Header: React.FC<HeaderProps> = ({ label, showBackArrow }) => {
 
   return (
     <div className="border-b-[1px] border-neutral-800 p-5">
-      <div className="flex flex-row items-center gap-2"></div>
+      <div className="flex flex-row items-center gap-2">
+        {showBackArrow && (
+          <BiArrowBack
+            onClick={handleBack}
+            color="white"
+            className="cursor-pointer hover:opacity-70 transition"
+            size={20}
+          />
+        )}
+        <h1 className="text-white text-xl font-semibold">{label}</h1>
+      </div>
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import { Layout } from "@/components/Layout";
+import Model from "@/components/Model";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Model
+        isOpen
+        title=""
+        onClose={() => {}}
+        onSubmit={() => {}}
+        actionLabel={""}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
