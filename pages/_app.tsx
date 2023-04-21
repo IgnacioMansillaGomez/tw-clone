e34ce84a0028eb1fe8f97d0feab164e2d171faee
IgnacioMansillaGomez <ignaciomansilladeveloper@gmail.com>
2023-04-21 02:11:38 -0300

    remove model

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
 import { Layout } from "@/components/Layout";
-import Model from "@/components/Model";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Model
-        isOpen
-        title=""
-        onClose={() => {}}
-        onSubmit={() => {}}
-        actionLabel={""}
-      />
+      <LoginModal />
+      <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
