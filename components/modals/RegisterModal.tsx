cce9d3223ffe27b87ae0ef269cfe8008f8196769
IgnacioMansillaGomez <ignaciomansilladeveloper@gmail.com>
2023-04-22 04:36:08 -0300

    session provider , create account , sign in

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -4,6 +4,10 @@ import Input from "../Input";
 import Modal from "../Modal";
 import useRegisterModal from "@/hooks/useRegisterModal";
 
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
+
 const RegisterModal = () => {
   const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
@@ -17,15 +21,26 @@ const RegisterModal = () => {
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
-      // TODO register AND login
 
+      await axios.post("/api/register", {
+        email,
+        password,
+        username,
+        name,
+      });
+
+      toast.success("Account created");
+      signIn("credentials", {
+        email,
+        password,
+      });
       registerModal.onClose();
     } catch (error) {
-      console.log(error);
+      toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
-  }, [registerModal]);
+  }, [registerModal, email, password, username, name]);
 
   const onToggle = useCallback(() => {
     if (isLoading) {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import { Layout } from "@/components/Layout";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
       <LoginModal />
       <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   );
 }
